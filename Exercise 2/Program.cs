2ecdcab995d52e1be9e79d2bc05864a6bb043fac
Felipe Ferreira <fefucho08@gmail.com>
2024-01-13 16:37:33 -0300

    Adicionado exercicio 2

---
+++ Exercise 2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************");
+            Console.WriteLine("Menu");
+            Console.WriteLine("1 - New Game");
+            Console.WriteLine("2 - Load Game");
+            Console.WriteLine("3 - Options");
+            Console.WriteLine("4 - Quit");
+            Console.WriteLine("**************");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("New game started!");
+                    break;
+                case 2:
+                    Console.WriteLine("Loading Game...");
+                    break;
+                case 3:
+                    Console.WriteLine("Showing Options");
+                    break;
+                case 4:
+                    Console.WriteLine("Exiting game... Thanks for playing!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+        }
+    }
+}
