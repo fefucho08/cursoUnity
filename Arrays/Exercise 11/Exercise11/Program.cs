4ac54a6fdea96e96910ae6c1475e548c9978ce15
Felipe Ferreira <fefucho08@gmail.com>
2024-01-20 19:52:49 -0300

    Adicionado exercicio 11

---
+++ Arrays/Exercise 11/Exercise11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ConsoleCards;
+
+namespace Exercise11
+{
+    /// <summary>
+    /// Exercise 11
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Exercise 11
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            Card[] cards = new Card[5];
+
+            deck.Shuffle();
+
+            cards[0] = deck.TakeTopCard();
+            cards[0].FlipOver();
+            cards[0].Print();
+
+            cards[1] = deck.TakeTopCard();
+            cards[1].FlipOver();
+            cards[0].Print();
+            cards[1].Print();
+
+        }
+    }
+}
